89bfe8d70b6fc570fd8b560b935dd20d94e0ab3e
Dave Brown <dbrown29@gmail.com>
2014-01-24 14:26:05 -0700

    don't install barcode generation software, we create our own shelf tags outside of this app

--- pos/installation/ubuntu/install-server.sh
+++ pos/installation/ubuntu/install-server.sh
@@ -8,7 +8,7 @@ echo "mysql-server mysql-server/root_password select $ROOT_PASSWORD" | debconf-s
 echo "mysql-server mysql-server/root_password_again select $ROOT_PASSWORD" | debconf-set-selections
 echo "python-mysqldb mysql-server/root_password select $ROOT_PASSWORD" | debconf-set-selections
 echo "python-mysqldb mysql-server/root_password_again select $ROOT_PASSWORD" | debconf-set-selections
-aptitude install -y mysql-server apache2 php5 libapache2-mod-php5 python-mysqldb php5-mysql php-image-barcode php-fpdf
+aptitude install -y mysql-server apache2 php5 libapache2-mod-php5 python-mysqldb php5-mysql
 
 python /pos/installation/install_server.py
 
